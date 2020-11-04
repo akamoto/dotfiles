495f254c976c3043200afc86e30f10a197bd3e72
Markus Mueller <mm@rbfh.de>
2020-11-04 15:18:43 +0100

    hammerspoon: add volume shortcut

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -174,6 +174,23 @@ hs.hotkey.bind({"ctrl", "alt"}, "i", function()
     hs.execute("$HOME/.bin/mac_map_keys")
 end)
 
+-- volume control
+function changeVolume(diff)
+  return function()
+    local current = hs.audiodevice.defaultOutputDevice():volume()
+    local new = math.min(100, math.max(0, math.floor(current + diff)))
+    if new > 0 then
+      hs.audiodevice.defaultOutputDevice():setMuted(false)
+    end
+    hs.alert.closeAll(0.0)
+    hs.alert.show("Volume " .. new .. "%", {}, 0.5)
+    hs.audiodevice.defaultOutputDevice():setVolume(new)
+  end
+end
+
+hs.hotkey.bind({'alt'}, 'c', changeVolume(-3))
+hs.hotkey.bind({'alt'}, 'v', changeVolume(3))
+
 -- set up your windowfilter
 --switcher = hs.window.switcher.new() -- default windowfilter: only visible windows, all Spaces
 --switcher_space = hs.window.switcher.new(hs.window.filter.new():setCurrentSpace(true):setDefaultFilter{}) -- include minimized/hidden windows, current Space only
